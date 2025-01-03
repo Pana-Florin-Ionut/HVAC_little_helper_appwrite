85389ff1c49c521e6e3416723b0817938f5b0ed6
Pana-Florin-Ionut <panaflorinionut@gmail.com>
2025-01-03 16:04:53 +0200

    Svelte not real

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,27 @@
+<script lang=ts>
+    import { databases } from '$lib/api';
+
+    let documents = [];
+
+    async function fetchDocuments() {
+        try {
+            const response = await databases.listDocuments('myDatabase', 'users');
+            documents = response.documents;
+        } catch (error) {
+            console.error('Error fetching documents:', error);
+        }
+    }
+
+    fetchDocuments();
+</script>
+
+<h1>Some Users</h1>
+<ul>
+    {#each documents as doc}
+        <li>{doc.name} ({doc.email})</li>
+    {/each}
+</ul>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
