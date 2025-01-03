a83cf7d422844e7fef8c08ecd49373d7e7ace3ff
Pana-Florin-Ionut <panaflorinionut@gmail.com>
2025-01-04 01:01:30 +0200

    update helper_function.py

--- appwrite/backend_scripts/helper_functions.py
+++ appwrite/backend_scripts/helper_functions.py
@@ -1,14 +1,17 @@
 from appwrite.services.databases import Databases
 from appwrite_data import client
 from appwrite_data import main_db_name
+from appwrite.query import Query
 databases = Databases(client)
 
-# print(databases.list())
-# print(type(databases.list()))
+
 def get_main_db_id():
-    content: dict = databases.list()
-    if content["total"] > 0:
-        for db in content["databases"]:
-            # print(db["name"])
-            if db["name"] == main_db_name:
-                return db["$id"]
+    # Query for the database with the name 'main_db_name'
+    try:
+        content = databases.list(queries=[Query.equal("name", main_db_name)])
+        # print(content)
+        if content["total"] > 0:
+            return content["databases"][0]["$id"]  # Return the ID of the first matching database
+    except Exception as e:
+        print(f"Error fetching database: {e}")
+    return None
